d985b98ad2641d7364971896334950cbb9df283e
ClaraFallDevMonster <clarah93@aol.de>
2022-08-12 19:38:23 +0200

    up

--- js/LoveGenerator.js
+++ js/LoveGenerator.js
@@ -1,10 +1,11 @@
 function lovegenerator() {
 var yourName = "";
-var Othername = "";
+var othername = "";
 yourName = document.getElementById("YourName").value;
 otherName = document.getElementById("OtherName").value;
 var loveScore = Math.random();
 loveScore = Math.floor(loveScore * 100) +1;
+
 
 if (loveScore > 80) {
   document.getElementById("prozent").innerHTML = yourName + " your Love Score with " + otherName + " is " + loveScore + "%";
