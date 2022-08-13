09ba3b4f39431a07c5f15b830c6704bdb4e6dbf8
ClaraFallDevMonster <clarah93@aol.de>
2022-08-13 10:24:23 +0200

    up

--- js/LoveGenerator.js
+++ js/LoveGenerator.js
@@ -16,23 +16,27 @@ var name1 = nameUpper1.toUpperCase() + nameLower1.toLowerCase();
 var name2 = nameUpper2.toUpperCase() + nameLower2.toLowerCase();
 
 if (yourName.length < 2 | otherName.length < 2) {
-    window.alert("You have to enter 2 names first!");
+    window.alert("Oops... You have to enter 2 names first!");
 }
 else if (loveScore > 80) {
-  document.getElementById("prozent").innerHTML = name1 + " your Love Score with " + name2 + " is " + loveScore + "%";
-  document.getElementById("text").innerHTML = "Der richtige Deckel für deinen hässlichen Topf. Kaufe dieser Person am besten sofort hübsche Unterwäsche um auf diesem Wege zu zeigen: Ich liebe dich trotz deines Übergewichts.";
+  document.getElementById("prozent").innerHTML = name1 + "'s Love Score with " + name2 + " is " + loveScore + "%";
+  document.getElementById("ueberschrift").innerHTML = "Are you fur real?";
+  document.getElementById("text").innerHTML = "You match purrfectly together. That's pawsome! You two have met a great purrsonality in each other and you're purrfect just the way you are. You really love each other, meow and forever!";
 }
 else if (loveScore > 60 && loveScore <= 80) {
-  document.getElementById("prozent").innerHTML = name1 + " your Love Score with " + name2 + " is " + loveScore + "%";
-  document.getElementById("text").innerHTML = "So weit so gut! Geh mal auf Tuchfühlung. Wenn's im Bett läuft, gib der Person eine Chance. Dringender Tipp: Wenn ihr mit dieser Person ein Sexvideo dreht, unterlegt es unbedingt mit Musik von irgendeinem Disney-Film! Falls es irgendwie doch den Weg ins Internet findet, werden die Disney-Anwälte schon dafür sorgen, dass es gelöscht wird.";
+  document.getElementById("prozent").innerHTML = name1 + "'s Love Score with " + name2 + " is " + loveScore + "%";
+  document.getElementById("ueberschrift").innerHTML = "So fur, so good.";
+  document.getElementById("text").innerHTML = "You are definitely the best furends in the whole wide world! If you work on your purrsonality a little, this can be a fantastic love story. Enjoy every meowment with each other. Best fishes for you!";
 }
 else if (loveScore > 30 && loveScore <= 60) {
-  document.getElementById("prozent").innerHTML = name1 + " your Love Score with " + name2 + " is " + loveScore + "%";
-  document.getElementById("text").innerHTML = "Ihr könnt ein Paar werden, aber ihr werdet viel Streiten, Teller werfen und euch mit Nudelhölzern verprügeln. Beziehung heißt, Kompromisse einzugehen und sich auf den Gegner einzustellen. Partner, ich meinte Partner! Solange er/sie gut im Bett ist, wirst du glücklich sein.";
+  document.getElementById("prozent").innerHTML = name1 + "'s Love Score with " + name2 + " is " + loveScore + "%";
+  document.getElementById("ueberschrift").innerHTML = "That's a pawsibility.";
+  document.getElementById("text").innerHTML = "Purrhaps you two can cuddle later with the right catitude. At the moment you are not quite settled in a relationship. But anything is pawsible. Stay pawsitive!";
 }
 else {
-  document.getElementById("prozent").innerHTML =  name1 + " your Love Score with " + name2 + " is " + loveScore + "%";
-  document.getElementById("text").innerHTML = "Diese Person ist nicht die richtige! Am besten einen Korb geben, eine Abfuhr, eine Backpfeife, einen Arschtritt. Meat Loafs “I would do anything for love, but I won’t do that” bezieht sich im übrigen auf die Weigerung, dem ostdeutschen Lebenspartner “Tomatensauce” aus Mehlschwitze und Ketchup zu machen. Falls diese Person allerdings Freundschaft Plus will - schlag zu.";
+  document.getElementById("prozent").innerHTML =  name1 + "'s Love Score with " + name2 + " is " + loveScore + "%";
+  document.getElementById("ueberschrift").innerHTML = "Now wait a meowment...";
+  document.getElementById("text").innerHTML = "Pawdon my language. You've got to be kitten me?! You guys don't match at all. You two together = a cat-astrophe!";
 }
 
 
